afc060cc26dcdf3bd0a801b4cbc6ddbf929bc121
Erik "Bardez" Frantz <bardez@bardez.net>
2013-03-03 22:10:56 +0000

    Added "AdjustForPacking" and "FlipVertical" to IPixelConverter
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C688

--- Lib AV/SWScale Pixel Converter.h
+++ Lib AV/SWScale Pixel Converter.h
@@ -7,6 +7,7 @@
 using namespace System;
 using namespace System::IO;
 using namespace Bardez::Projects::Multimedia::MediaBase::Data::Pixels;
+using namespace Bardez::Projects::Multimedia::MediaBase::Frame::Image;
 
 
 namespace Bardez
@@ -33,6 +34,21 @@ namespace Bardez
 					/// <param name="decodedBitDepth">The bits per pixel once decoded</param>
 					/// <returns>New byte data</returns>
 					virtual MemoryStream^ ConvertData(MemoryStream^ data, Bardez::Projects::Multimedia::MediaBase::Data::Pixels::Enums::PixelFormat sourceFormat, Bardez::Projects::Multimedia::MediaBase::Data::Pixels::Enums::PixelFormat destinationFormat, Int32 horizontalPacking, Int32 verticalPacking, Int32 sourceWidth, Int32 sourceHeight, Int32 decodedBitDepth);
+
+					/// <summary>Flips pixel data scan line by scan line</summary>
+					/// <param name="data">Decompressed data to flip</param>
+					/// <param name="metadata">Image metadata for the source frame</param>
+					/// <returns>The vertically flipped data</returns>
+					/// <remarks>Uses the current pixel data's packing, not the destination's</remarks>
+					virtual MemoryStream^ FlipVertical(MemoryStream^ data, ImageMetadata^ metadata);
+
+					/// <summary>Adjusts the packing bytes of the data</summary>
+					/// <param name="data">Data to pack</param>
+					/// <param name="metadata">Image metadata for the source frame</param>
+					/// <param name="destPackingHorizontal">target horizontal packing</param>
+					/// <param name="destPackingVertical">target vertical packing</param>
+					/// <returns>The adjusted packed bytes</returns>
+					virtual MemoryStream^ AdjustForPacking(MemoryStream^ data, ImageMetadata^ metadata, Int32 destPackingHorizontal, Int32 destPackingVertical);
 				#pragma endregion
 				};
 			}
