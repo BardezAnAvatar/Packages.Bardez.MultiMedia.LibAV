ce80eb2780f180b04967ada62f621f81a8fbe894
Erik "Bardez" Frantz <bardez@bardez.net>
2019-08-12 23:15:13 -0500

    Adding assembly notations

---
+++ Bardez.MultiMedia.LibAV/AssemblyInfo.cpp
@@ -0,0 +1,18 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute("Bardez.MultiMedia.LibAV")];
+[assembly:AssemblyDescriptionAttribute("Lib AV interface for .NET")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("Erik \"Bardez\" Frantz")];
+[assembly:AssemblyProductAttribute("Bardez.MultiMedia.LibAV")];
+[assembly:AssemblyCopyrightAttribute("Copyright © 2019")];
+[assembly:AssemblyTrademarkAttribute("Inc")];
+[assembly:AssemblyCultureAttribute("")];
+[assembly:AssemblyVersionAttribute("0.7.15.0")];
+[assembly:ComVisible(false)];
+[assembly:CLSCompliantAttribute(true)];
+[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
